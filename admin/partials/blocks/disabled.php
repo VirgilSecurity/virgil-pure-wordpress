d72ea976742a3d29915db723e0cd61b0bdcd2fd4
Petrenko Anton <antptrnk@gmail.com>
2019-04-10 12:52:31 +0300

    Fix link (extension missing block)

--- admin/partials/blocks/disabled.php
+++ admin/partials/blocks/disabled.php
@@ -6,8 +6,7 @@ use VirgilSecurityPure\Config\Config;
     <div class="virgil-phe-failed-content">
         <h2 class="virgil-phe-failed-title">EXTENSION MISSING</h2>
         <p class="virgil-phe-failed-desc">
-            <?= Config::EXTENSION_NAME ?> can't be found. Please check <a href="https://github
-            .com/VirgilSecurity/virgil-pure-wordpress#step-1-add-the-vsce_phe_php-extension-into-your-server-before-using-the-plugin" target="_blank">this guide</a> for more information.
+            <?= Config::EXTENSION_NAME ?> can't be found. Please check <a href="https://github.com/VirgilSecurity/virgil-pure-wordpress#step-1-add-the-vsce_phe_php-extension-into-your-server-before-using-the-plugin" target="_blank">this guide</a> for more information.
         </p>
     </div>
 </div>
