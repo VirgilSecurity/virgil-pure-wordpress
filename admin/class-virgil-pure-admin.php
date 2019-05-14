437c5cc4d56f2e4930146529ba0c1e8f5d93205a
Petrenko Anton <antptrnk@gmail.com>
2019-05-14 15:39:42 +0300

    Resolve conflicts with the master

--- admin/class-virgil-pure-admin.php
+++ admin/class-virgil-pure-admin.php
@@ -55,7 +55,7 @@ class Virgil_Pure_Admin
     public function enqueue_styles()
     {
         wp_enqueue_style($this->Virgil_Pure, plugin_dir_url(__FILE__) . 'css/virgil-pure-admin.css', array(),
-            $this->version.time(), 'all');
+            $this->version, 'all');
     }
 
     /**
