866c25119280f3266998e70863c3235fb96590b3
Petrenko Anton <antptrnk@gmail.com>
2019-05-28 12:50:15 +0300

    Update README.txt and FAQ page

--- README.txt
+++ README.txt
@@ -51,15 +51,13 @@ Or from the official GitHub repository:
 
 = Do users have to change their passwords if the database has been compromised? =
 
-If a database has been stolen, users do not need to change their original passwords. However, you need to rotate all user records in your database. This will use cryptography to disconnect the compromised Pure records from the original passwords, leaving any unauthorized party empty handed.
+If a database has been stolen, users do not need to change their original passwords. However, you will need to rotate all user records in your database. This will use cryptography to disconnect the compromised Pure records from the original passwords, leaving any unauthorized party empty handed.
 
 = How does the Recovery Key work? =
 
-Recovery Key is a key pair that allows you to recover users password hashes if you ever need to deactivate the Pure
-plugin. The Recovery Key encrypts the password hashes, and stores the encrypted values into the wp_usermeta table in your database.
+Recovery Key is a key pair that allows you to recover the original user password hashes if you ever need to deactivate the Pure plugin. The Recovery Key encrypts the password hashes, and stores the encrypted values into the wp_usermeta table in your database.
 
-The Recovery Key utilizes a public and private key pair. The public key is stored in the wp_option table and
-the Private Key must be stored by you securely on an external device.
+The Recovery Key utilizes a public and private key pair. The public key is stored in the wp_option table and the Private Key must be stored by you securely on an external device.
 
 = How much does it cost? =
 
@@ -67,7 +65,7 @@ Pure is a FREE toolkit. All libraries are open source and can be found on GitHub
 
 = What if an App Private Key gets lost? =
 
-There is no way to restore the APP_SECRET_KEY. The database becomes inaccessible and therefore useless. So, it makes sense to immediately make a backup of the key in any convenient form.
+There is no way to restore the `APP_SECRET_KEY`. The database records become inaccessible and therefore useless. So, it is highly recommended that you immediately create a backup of the key in a secure location to avoid losing it.
 
 == Screenshots ==
 
@@ -80,6 +78,7 @@ There is no way to restore the APP_SECRET_KEY. The database becomes inaccessible
 available for you)
 * Remove Demo Mode
 * Minor fixes and optimization
+* Required virgil_crypto_php extension
 
 = 0.1.2 =
 * Fix activation error
--- admin/partials/blocks/faq.php
+++ admin/partials/blocks/faq.php
@@ -3,24 +3,30 @@
     <p class="virgil-pure-global-faq-question">Do users have to change their passwords if the database has been
         compromised?</p>
     <p class="virgil-pure-global-faq-answer">If a database has been stolen, users do not need to change their original
-        passwords. However, you need to
-        rotate all user records in your database. This will use cryptography to disconnect the compromised Pure records from the original passwords, leaving any unauthorized party empty handed.</p>
+        passwords. However, you will need to rotate all user records in your database. This will use cryptography to
+        disconnect the compromised Pure records from the original passwords, leaving any unauthorized party empty
+        handed.</p>
     <hr>
     <p class="virgil-pure-global-faq-question">How does the Recovery Key work?</p>
-    <p class="virgil-pure-global-faq-answer">Recovery Key is a key pair that allows you to recover users password
-        hashes if you ever need to deactivate the Pure plugin. The Recovery Key encrypts the password hashes, and
-        stores the encrypted values into the wp_usermeta table in your database.
+    <p class="virgil-pure-global-faq-answer">Recovery Key is a key pair that allows you to recover the original user
+        password hashes if you ever need to deactivate the Pure plugin. The Recovery Key encrypts the password hashes,
+        and stores the encrypted values into the wp_usermeta table in your database.
         <br>
-        The Recovery Key utilizes a public and private key pair. The public key is stored in the wp_option table and
-        the Private Key must be stored by you securely on an external device.
+        The Recovery Key utilizes a public and private key pair. The public key is stored in the wp_option table and the
+        Private Key must be stored by you securely on an external device.
     </p>
     <hr>
     <p class="virgil-pure-global-faq-question">How much does it cost?</p>
-    <p class="virgil-pure-global-faq-answer">Pure is a FREE toolkit. All libraries are open source and can be found on GitHub, where they are available for free to any user.</p>
+    <p class="virgil-pure-global-faq-answer">Pure is a FREE toolkit. All libraries are open source and can be found on
+        GitHub, where they are available for free to any user.</p>
     <hr>
     <p class="virgil-pure-global-faq-question">What if an App Private Key gets lost?</p>
-    <p class="virgil-pure-global-faq-answer">There is no way to restore the  APP_SECRET_KEY. The database becomes
-        inaccessible and therefore useless. So, it makes sense to immediately make a backup of the key in any convenient form.</p>
+    <p class="virgil-pure-global-faq-answer">There is no way to restore the `APP_SECRET_KEY`. The database records
+        become inaccessible and therefore useless. So, it is highly recommended that you immediately create a backup of
+        the key in a secure location to avoid losing it.
+    </p>
     <hr>
-    <p class="virgil-pure-global-faq-answer"><a href="https://developer.virgilsecurity.com/docs/use-cases/v1/passwords-and-data-protection?_ga=2.124234441.238283218.1553788809-677779594.1553687510" target="_blank">Learn more from our documentation</a></p>
+    <p class="virgil-pure-global-faq-answer"><a
+                href="https://developer.virgilsecurity.com/docs/use-cases/v1/passwords-and-data-protection?_ga=2.124234441.238283218.1553788809-677779594.1553687510"
+                target="_blank">Learn more from our documentation</a></p>
 </div>
